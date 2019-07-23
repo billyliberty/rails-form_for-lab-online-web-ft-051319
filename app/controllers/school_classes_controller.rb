fe5f6175cbbb35905fb2c2c9207382c3f6ec081e
Joshua Crum <joshua.crum@gmail.com>
2019-07-23 19:42:14 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,9 +1,12 @@
 class SchoolClassesController < ApplicationController
 
   def new
+    @school_class = SchoolClass.new
   end
 
   def create
+    @school_class = SchoolClass.create(school_class_params)
+    redirect_to school_class_path(@school_class)
   end
 
   def show
