bd4e1bc98806fe4706ff6b3afc39f12fc0b93b85
Joshua Crum <joshua.crum@gmail.com>
2019-07-23 19:39:33 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,17 +4,36 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.create(student_params)
+    redirect_to student_path(@student)
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    redirect_to student_path(@student)
   end
+
+  private
+
+    def student_params
+      params.require(:student).permit(:first_name, :last_name)
+    end
+
+    def student_find
+      @student = Student.find(params[:id])
+    end
+    
 end
